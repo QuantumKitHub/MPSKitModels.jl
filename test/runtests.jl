da7123aeb0fe544eeb0baa5839d3f2fb6440210d
maarten <maarten.vandamme@ugent.be>
2020-09-09 15:37:08 +0200

    changed workflows

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,3 +1,11 @@
-using TensorKit,MPSKit,MPSKitModels
+using Test,TensorKit,MPSKit,MPSKitModels
 
-# ...
+@testset "xxz" begin
+    @testset "nonsym_xxz_ham" begin
+        th = MPSKitModels.nonsym_xxz_ham()
+        ts = InfiniteMPS([ℂ^3],[ℂ^48]);
+        (ts,pars,_) = find_groundstate(ts,th,Vumps(maxiter=400,verbose=false));
+        (energies,Bs) = quasiparticle_excitation(th,Float64(pi),ts,pars);
+        @test energies[1] ≈ 0.41047925 atol=1e-4
+    end
+end

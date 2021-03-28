f7fe0905e070cf9b550727dc3fb7075279f93323
maarten <maarten.vandamme@ugent.be>
2021-03-28 01:08:52 +0100

    bump version - fix test

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,7 +5,7 @@ using Test,TensorKit,MPSKit,MPSKitModels
         th = MPSKitModels.nonsym_xxz_ham()
         ts = InfiniteMPS([ℂ^3],[ℂ^48]);
         (ts,pars,_) = find_groundstate(ts,th,Vumps(maxiter=400,verbose=false));
-        (energies,Bs) = quasiparticle_excitation(th,Float64(pi),ts,pars);
+        (energies,Bs) = excitations(th,QuasiparticleAnsatz(),Float64(pi),ts,pars);
         @test energies[1] ≈ 0.41047925 atol=1e-4
     end
 end

1e54b5310be6266ac2539fdb385913a9e8e30739
lkdvos <lukas.devos@ugent.be>
2023-05-20 15:09:44 +0200

    Add docstring HoneycombYC

--- src/lattices/triangularlattice.jl
+++ src/lattices/triangularlattice.jl
@@ -1,3 +1,9 @@
+"""
+    HoneycombYC(L::Integer, N::Integer=L)
+
+A honeycomb lattice on an infinite cylinder with `L` sites per rung and `N` sites per unit cell.
+The y-axis is aligned along an edge of the hexagons, and the circumference is ``3L/4``.
+"""
 struct HoneycombYC <: AbstractLattice{2}
     L::Int
     N::Int

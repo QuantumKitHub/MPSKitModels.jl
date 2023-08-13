05a4ad5821617e9ac84b2f659a08076a92d497e6
lkdvos <lukas.devos@ugent.be>
2023-08-13 18:11:18 +0200

    Fix deprecation warnings

--- src/models/transfermatrices.jl
+++ src/models/transfermatrices.jl
@@ -57,7 +57,7 @@ function sixvertex(elt::Type{<:Number}=ComplexF64, ::Type{Trivial}=Trivial; a=1.
          0 c b 0
          0 b c 0
          0 0 0 a]
-    return DenseMPO(permute(TensorMap(d, ℂ^2 ⊗ ℂ^2, ℂ^2 ⊗ ℂ^2), (1, 2), (4, 3)))
+    return DenseMPO(permute(TensorMap(d, ℂ^2 ⊗ ℂ^2, ℂ^2 ⊗ ℂ^2), ((1, 2), (4, 3))))
 end
 function sixvertex(elt::Type{<:Number}, ::Type{U1Irrep};
                    a=1.0, b=1.0, c=1.0)
@@ -66,7 +66,7 @@ function sixvertex(elt::Type{<:Number}, ::Type{U1Irrep};
     blocks(mpo)[Irrep[U₁](0)] = [b c; c b]
     blocks(mpo)[Irrep[U₁](1)] = reshape([a], (1, 1))
     blocks(mpo)[Irrep[U₁](-1)] = reshape([a], (1, 1))
-    return DenseMPO(permute(mpo, (1, 2), (4, 3)))
+    return DenseMPO(permute(mpo, ((1, 2), (4, 3))))
 end
 function sixvertex(elt::Type{<:Number}, ::Type{CU1Irrep};
                    a=1.0, b=1.0, c=1.0)
@@ -75,7 +75,7 @@ function sixvertex(elt::Type{<:Number}, ::Type{CU1Irrep};
     blocks(mpo)[Irrep[CU₁](0, 0)] = reshape([b + c], (1, 1))
     blocks(mpo)[Irrep[CU₁](0, 1)] = reshape([-b + c], (1, 1))
     blocks(mpo)[Irrep[CU₁](1, 2)] = reshape([a], (1, 1))
-    return DenseMPO(permute(mpo, (1, 2), (4, 3)))
+    return DenseMPO(permute(mpo, ((1, 2), (4, 3))))
 end
 
 #===========================================================================================

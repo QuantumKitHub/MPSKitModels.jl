7a0bc47d3d8f44ce50f787c457228be6819bb407
lkdvos <lukas.devos@ugent.be>
2024-02-06 11:37:05 +0100

    rename variables

--- test/heisenberg.jl
+++ test/heisenberg.jl
@@ -7,37 +7,37 @@ E₁ = 0.41047925
 
 @testset "xxx" begin
     H = @inferred heisenberg_XXX()
-    Ψ = InfiniteMPS([ComplexSpace(3)], [ComplexSpace(48)])
-    @test sum(abs.(imag.(expectation_value(Ψ, H)))) ≈ 0 atol = 1e-10
-    Ψ, envs, δ = find_groundstate(Ψ, H, alg)
-    @test E₀ ≈ sum(expectation_value(Ψ, H, envs)) atol = 1e-2
+    ψ = InfiniteMPS([ComplexSpace(3)], [ComplexSpace(48)])
+    @test sum(abs.(imag.(expectation_value(ψ, H)))) ≈ 0 atol = 1e-10
+    ψ, envs, δ = find_groundstate(ψ, H, alg)
+    @test E₀ ≈ sum(expectation_value(ψ, H, envs)) atol = 1e-2
 
-    ΔEs, qps = excitations(H, QuasiparticleAnsatz(), Float64(pi), Ψ, envs)
+    ΔEs, qps = excitations(H, QuasiparticleAnsatz(), Float64(pi), ψ, envs)
     @test E₁ ≈ first(ΔEs) atol = 1e-2
 end
 
 @testset "xxx SU2" begin
     H = @inferred heisenberg_XXX(SU2Irrep)
-    Ψ = InfiniteMPS([Rep[SU₂](1 => 1)], [Rep[SU₂](1 // 2 => 5, 3 // 2 => 5, 5 // 2 => 1)])
-    @test sum(abs.(imag.(expectation_value(Ψ, H)))) ≈ 0 atol = 1e-10
-    Ψ, envs, δ = find_groundstate(Ψ, H, alg)
-    @test E₀ ≈ sum(expectation_value(Ψ, H, envs)) atol = 1e-2
+    ψ = InfiniteMPS([Rep[SU₂](1 => 1)], [Rep[SU₂](1 // 2 => 5, 3 // 2 => 5, 5 // 2 => 1)])
+    @test sum(abs.(imag.(expectation_value(ψ, H)))) ≈ 0 atol = 1e-10
+    ψ, envs, δ = find_groundstate(ψ, H, alg)
+    @test E₀ ≈ sum(expectation_value(ψ, H, envs)) atol = 1e-2
 
-    ΔEs, qps = excitations(H, QuasiparticleAnsatz(), Float64(pi), Ψ, envs;
+    ΔEs, qps = excitations(H, QuasiparticleAnsatz(), Float64(pi), ψ, envs;
                            sector=SU2Irrep(1))
     @test E₁ ≈ first(ΔEs) atol = 1e-2
 end
 
 @testset "xxx U1" begin
     H = @inferred heisenberg_XXX(U1Irrep)
-    Ψ = InfiniteMPS([Rep[U₁](0 => 1, 1 => 1, -1 => 1)],
+    ψ = InfiniteMPS([Rep[U₁](0 => 1, 1 => 1, -1 => 1)],
                     [Rep[U₁](1 // 2 => 10, -1 // 2 => 10, 3 // 2 => 5, -3 // 2 => 5,
                              5 // 2 => 3, -5 // 2 => 3)])
-    @test sum(abs.(imag.(expectation_value(Ψ, H)))) ≈ 0 atol = 1e-10
-    Ψ, envs, δ = find_groundstate(Ψ, H, alg)
-    @test E₀ ≈ sum(expectation_value(Ψ, H, envs)) atol = 1e-2
+    @test sum(abs.(imag.(expectation_value(ψ, H)))) ≈ 0 atol = 1e-10
+    ψ, envs, δ = find_groundstate(ψ, H, alg)
+    @test E₀ ≈ sum(expectation_value(ψ, H, envs)) atol = 1e-2
 
-    ΔEs, qps = excitations(H, QuasiparticleAnsatz(), Float64(pi), Ψ, envs;
+    ΔEs, qps = excitations(H, QuasiparticleAnsatz(), Float64(pi), ψ, envs;
                            sector=U1Irrep(1))
     @test E₁ ≈ first(ΔEs) atol = 1e-2
 end

95647697820d9cded8436d3a1ba12b9cdf0b8d5b
Boris De Vos <boris.devos@ugent.be>
2024-10-15 15:06:06 +0200

    clean up theoretical energy function

--- test/potts.jl
+++ test/potts.jl
@@ -4,12 +4,10 @@ using TensorKit
 alg = VUMPS(; maxiter=25, verbosity=0)
 
 # https://iopscience.iop.org/article/10.1088/0305-4470/14/11/020/meta
-E₀3 = -(4 / 3 + 2sqrt(3) / π)
-E₀4 = 2 - 8*log(2)
-function E₀(Q)
-    @assert Q > 4
-    ν = acosh(sqrt(Q)/2)
-    summation = sum((-1)^n / (cosh(ν) - cosh((2*n+1)*ν)) for n in 1:1000)
+function E₀(Q::Int, maxiter::Int = 1000)
+    Q == 3 && return -(4 / 3 + 2sqrt(3) / π)
+    Q == 4 && return 2 - 8*log(2)
+    summation = sum((-1)^n / (sqrt(Q)/2 - cosh((2*n+1)*acosh(sqrt(Q)/2))) for n in 1:maxiter)
     limit = 2 - Q - sqrt(Q)*(Q-4)*summation
     return limit
 end
@@ -20,7 +18,7 @@ end
     ψ = InfiniteMPS(3, 32)
     @test imag(expectation_value(ψ, H)) ≈ 0 atol = 1e-10
     ψ, envs, δ = find_groundstate(ψ, H, alg)
-    @test E₀3 ≈ expectation_value(ψ, H, envs) atol = 1e-2
+    @test E₀(3) ≈ expectation_value(ψ, H, envs) atol = 1e-2
 end
 
 @testset "Z3Irrep" begin
@@ -28,7 +26,7 @@ end
     ψ = InfiniteMPS(Rep[ℤ₃](i => 1 for i in 0:2), Rep[ℤ₃](i => 12 for i in 0:2))
     @test imag(expectation_value(ψ, H)) ≈ 0 atol = 1e-10
     ψ, envs, δ = find_groundstate(ψ, H, alg)
-    @test E₀3 ≈ expectation_value(ψ, H, envs) atol = 1e-2
+    @test E₀(3) ≈ expectation_value(ψ, H, envs) atol = 1e-2
 end
 
 # Q = 4
@@ -37,7 +35,7 @@ end
     ψ = InfiniteMPS(4, 45)
     @test imag(expectation_value(ψ, H)) ≈ 0 atol = 1e-10
     ψ, envs, δ = find_groundstate(ψ, H, alg)
-    @test E₀4 ≈ expectation_value(ψ, H, envs) atol = 1e-2
+    @test E₀(4) ≈ expectation_value(ψ, H, envs) atol = 1e-2
 end
 
 @testset "Z4Irrep" begin
@@ -45,7 +43,7 @@ end
     ψ =  InfiniteMPS(Vect[Z4Irrep](i => 1 for i in 0:3), Vect[Z4Irrep](i => 12 for i in 0:3));
     @test imag(expectation_value(ψ, H)) ≈ 0 atol = 1e-10
     ψ, envs, δ = find_groundstate(ψ, H, alg)
-    @test E₀4 ≈ expectation_value(ψ, H, envs) atol = 1e-2
+    @test E₀(4) ≈ expectation_value(ψ, H, envs) atol = 1e-2
 end
 
 # Q = 5

9dd4b398b847b84ad6782701b62f1a534ac14417
victor <victor.vanthilt@gmail.com>
2024-10-29 09:11:29 +0100

    `AbstractTensorMap` typesignature changes

--- src/utility.jl
+++ src/utility.jl
@@ -12,19 +12,27 @@ end
 
 contract two single-site operators inta a single-site operator.
 """
-function contract_onesite(L::AbstractTensorMap{<:Any,1,2}, R::AbstractTensorMap{<:Any,2,1})
+function contract_onesite(L::AbstractTensorMap{<:Number,<:Any,1,2},
+                          R::AbstractTensorMap{<:Number,<:Any,2,1})
     @plansor H[-1; -2] := L[-1; 1 2] * τ[1 2; 3 4] * R[3 4; -2]
     return H
 end
-contract_onesite(L::AbstractTensorMap{<:Any,1,1}, R::AbstractTensorMap{<:Any,1,1}) = L * R
+function contract_onesite(L::AbstractTensorMap{<:Number,<:Any,1,1},
+                          R::AbstractTensorMap{<:Number,<:Any,1,1})
+    return L * R
+end
 
 """
     contract_twosite(L, R)
 
 contract two single-site operators into a two-site operator.
 """
-function contract_twosite(L::AbstractTensorMap{<:Any,1,2}, R::AbstractTensorMap{<:Any,2,1})
+function contract_twosite(L::AbstractTensorMap{<:Number,<:Any,1,2},
+                          R::AbstractTensorMap{<:Number,<:Any,2,1})
     @plansor H[-1 -2; -3 -4] := L[-1; -3 1] * R[1 -2; -4]
     return H
 end
-contract_twosite(L::AbstractTensorMap{<:Any,1,1}, R::AbstractTensorMap{<:Any,1,1}) = L ⊗ R
+function contract_twosite(L::AbstractTensorMap{<:Number,<:Any,1,1},
+                          R::AbstractTensorMap{<:Number,<:Any,1,1})
+    return L ⊗ R
+end
